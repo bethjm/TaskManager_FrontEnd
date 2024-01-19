cbd499f3717d91fd2f5c8519abfbef098f3b84e1
bethjm <b.jeanne.mills@gmail.com>
2024-01-19 17:03:00 -0500

    create added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ViewDayPage from "./components/ViewDayPage";
 
 function App() {
   // read
+  //working in console 5:01
   fetch("http://localhost:3009/api/tasks")
     .then((response) => response.json())
     .then((data) => {
@@ -15,6 +16,20 @@ function App() {
       console.log("inside fetch request");
     })
     .catch((error) => console.error("There be an error:", error));
+
+  //create
+  fetch("http://localhost:3009/api/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.error("Error:", error));
 
   return (
     <div className="App">
