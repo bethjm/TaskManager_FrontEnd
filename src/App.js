faf75a1bfb0b9b55df317f38b211770d5a01c2f2
bethjm <b.jeanne.mills@gmail.com>
2024-01-19 17:00:48 -0500

    fetch all items working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import MainPage from "./components/MainPage";
 import UpdatePage from "./components/UpdatePage";
@@ -8,6 +7,15 @@ import ViewAllPage from "./components/ViewAllPage";
 import ViewDayPage from "./components/ViewDayPage";
 
 function App() {
+  // read
+  fetch("http://localhost:3009/api/tasks")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log("inside fetch request");
+    })
+    .catch((error) => console.error("There be an error:", error));
+
   return (
     <div className="App">
       <h1>hi from app</h1>
