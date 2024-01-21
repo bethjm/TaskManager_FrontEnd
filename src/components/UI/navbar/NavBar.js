e9c6a5a2e92169bda81d2494382f4e5cfd272d2d
bethjm <b.jeanne.mills@gmail.com>
2024-01-20 19:50:23 -0500

    added basic styling for nav bar

--- src/components/UI/navbar/NavBar.js
+++ src/components/UI/navbar/NavBar.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./NavBar.css";
 
+import Add from "../images/add_sign.png";
+import Alert from "../images/Alert.png";
+import Today from "../images/Calendar.png";
+
 function NavBar() {
   return (
     <div className="NavBar">
       <div className="NavBar_Container">
-        <h2>hi from NavBar</h2>
+        <div className="nav_icons">
+          <a>
+            <img src={Add} />
+          </a>
+          <a>
+            <img src={Alert} />
+          </a>
+          <a>
+            <img src={Today} />
+          </a>
+        </div>
       </div>
     </div>
   );
