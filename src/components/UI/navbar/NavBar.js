6f2a23bdf9f6d1331e415f1a8cf19656ec6eb8df
bethjm <b.jeanne.mills@gmail.com>
2024-01-20 20:30:11 -0500

    finished design ffor navbar

--- src/components/UI/navbar/NavBar.js
+++ src/components/UI/navbar/NavBar.js
@@ -4,20 +4,21 @@ import "./NavBar.css";
 import Add from "../images/add_sign.png";
 import Alert from "../images/Alert.png";
 import Today from "../images/Calendar.png";
+import Circle from "../images/circle.png";
 
 function NavBar() {
   return (
     <div className="NavBar">
       <div className="NavBar_Container">
         <div className="nav_icons">
-          <a>
+          <a className="icon_container" href="/api/tasks/viewday">
+            <img src={Today} />
+          </a>
+          <a className="icon_container" href="/api/tasks/addnew">
             <img src={Add} />
           </a>
-          <a>
+          <a className="icon_container" href="/">
             <img src={Alert} />
-          </a>
-          <a>
-            <img src={Today} />
           </a>
         </div>
       </div>
